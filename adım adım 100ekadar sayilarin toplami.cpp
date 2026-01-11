34ce836ae63c026ac1ab553264b925e5e7c16354
Arda <ardaferrari07@gmail.com>
2026-01-11 23:33:26 +0300

    Add files via upload

---
+++ adım adım 100ekadar sayilarin toplami.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int sayac;
+    int toplam=0;
+    char harf[]="cik";
+    for(sayac=0; sayac<=100; sayac++){
+    	toplam+=sayac;
+    	printf("\n%d ==> %d.adim",toplam,sayac);
+	}
+    printf("     0'dan 100'e kadar olan sayilarin toplami %d",toplam);
+}
